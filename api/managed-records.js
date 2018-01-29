f34c3ea445d62cad2fcaf16b76fcb0d6fb276da6
Jason Sadler <jason.r.sadler@gmail.com>
2018-01-29 10:33:36 -0500

    clean up managed-records

--- api/managed-records.js
+++ api/managed-records.js
@@ -13,64 +13,69 @@ function handleErrors(response) {
 }
 
 function retrieve(...args) {
-    let qParams = args[0];  
-    var uriObj = new URI(window.path),
-    page = (qParams && qParams.page) ? qParams.page : 1,
-    colors = (qParams && qParams.colors) ? qParams.colors : null,
-    offset = ((page && page != 1) ? page-1 : 0) * 10;
-    uriObj.addSearch({'limit': 10, 'offset': offset})
-    if (colors) {
-        uriObj.addSearch({'color[]': colors});
-    }
+    var uri = constructURI(args[0]);
     return new Promise(function(resolve, reject) {
-        
-        fetch(URI(uriObj))
-        .then(r => r.json())
+        fetch(URI(uri.uriObj))
+        .then(res => res.json())
         .catch(err => console.log(err))
-        .then(function(res)  {  
-            uriObj.removeSearch('offset');
-            uriObj.addSearch({'offset': offset + 10});
-            fetch(URI(uriObj)).then(r => r.json())
+        .then(res =>  {  
+            peekNext(uri)
             .then(peek => {             
                 var payload = { 
                     ids: res.map(idMap => { return idMap.id }),
                     open: 
-                    res.filter(filter => {
-                        return filter.disposition === 'open'
-                    })
-                    .map(map => { 
-                        return  {                                  
-                            id: map.id, 
-                            color: map.color, 
-                            disposition: map.disposition, 
-                            isPrimary: isPrimaryColor(map.color) 
-                        } 
-                    }),
+                        res.filter(filter => {
+                            return filter.disposition === 'open'
+                        })
+                        .map(map => { 
+                            return  {                                  
+                                id: map.id, 
+                                color: map.color, 
+                                disposition: map.disposition, 
+                                isPrimary: isPrimaryColor(map.color) 
+                            } 
+                        }),
                     closedPrimaryCount: 
-                    res.filter(filter => {
-                        return filter.disposition === 'closed' && 
-                        isPrimaryColor(filter.color);                        
-                    })
-                    .length,
-                    previousPage: page == 1 || page == NaN ? null : page-1,                
-                    nextPage: peek.length > 0 ? page+1 : null 
-                    
-                    
+                        res.filter(filter => {
+                            return filter.disposition === 'closed' && 
+                            isPrimaryColor(filter.color);                        
+                        }).length,
+                    previousPage: uri.page == 1 || uri.page == NaN ? null : uri.page-1,                
+                    nextPage: peek.length > 0 ? uri.page+1 : null 
                 };
                 return resolve(payload);
-                
             })
             .catch(err => {
                 console.log(err);
-                resolve(err);
-                
+                resolve(err);                
             });
         });       
     });
 }
 
+function peekNext(uri) {
+    uri.uriObj.removeSearch('offset');
+    uri.uriObj.addSearch({'offset': uri.offset + 10});
+    return fetch(URI(uri.uriObj)).then(res => res.json())
+}
+
+function constructURI(params) {
+    uriObj = new URI(window.path);        
+    var constructed = {
+        uriObj: uriObj,
+        page: (params && params.page) ? params.page : 1,
+        colors: (params && params.colors) ? params.colors : null,
+        offset: ((params && params.page && params.page != 1) ? params.page-1 : 0) * 10    
+    };
+    constructed.uriObj.addSearch({'limit': 10, 'offset': constructed.offset});
+    if (constructed.colors) {
+        uriObj.addSearch({'color[]': constructed.colors});
+    }
+    return constructed;
+}
+
 function isPrimaryColor(color) {
-    return color === 'red' || color === 'yellow' || color === 'blue';
+    return ['red', 'yellow', 'blue'].includes(color);
 }
 
 
